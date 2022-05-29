780c99ac87465e5097e125a633f27fb377387f70
Batmanatorul <batmanatorul@gmail.com>
2022-05-30 01:08:33 +0300

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import com.batmanatorul.api.JavaDatapack;
+import com.batmanatorul.api.annotation.Datapack;
+//The name variable will be deleted in the next version
+@Datapack(name = "TestPack", id = "tp", description = "description")
+public class Main extends JavaDatapack {
+    @Override
+    public void onEnable() {
+
+    }
+}
